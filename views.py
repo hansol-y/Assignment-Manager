4c3793bda4fbe6f8fd2c3fe73dc2f28aeaa2e743
HS940405 <hansol.yang.94@gmail.com>
2022-08-23 15:23:16 -0700

    Adding pages

--- views.py
+++ views.py
@@ -4,6 +4,12 @@ views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("main.html")
 
+@views.route("/login")
+def login():
+    return render_template("login.html")
 
+@views.route("/register")
+def register():
+    return render_template("register.html")
